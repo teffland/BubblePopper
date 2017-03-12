5c78a8a84dd6a4447f8e51e3ee623785d9469911
teffland <tom.effland@gmail.com>
2017-03-12 18:50:16 -0400

    ened to end popup

--- src/chrome/content.js
+++ src/chrome/content.js
@@ -1,2 +1,28 @@
-var url = chrome.runtime.getURL();
-console.log(url);
+$(function() {
+  console.log("Reading page url and sending to Backend");
+  var url = window.location.href;
+  console.log("Detected url", url);
+  console.log("sending to server")
+  var data;
+  $.get({
+    url: "https://localhost:5000/read_url",
+    data: {"url":url},
+    success: function (data) {
+      console.log('content data', data);
+      chrome.runtime.onMessage.addListener(
+        function(message, sender, sendResponse) {
+            switch(message.type) {
+                case "getData":
+                    console.log('getData' ,data);
+                    sendResponse(data);
+                    break;
+                default:
+                    console.error("Unrecognised message: ", message);
+            }
+        });
+      }
+    });
+
+
+
+});
